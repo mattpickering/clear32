9096ec47342e26478db48f9c50a983d116db7175
Matt Pickering <mattpickering@live.com>
2020-04-05 18:25:52 +1000

    Removed cleanup from build.bat

--- build.bat
+++ build.bat
@@ -1,7 +1,3 @@
 @echo off
-del .\src\*.exe >nul
-del .\src\*.ilk >nul
-del .\src\*.obj >nul
-del .\src\*.pdb >nul
 if not exist "./bin" mkdir bin
 cl ./src/main.cpp /EHsc /Fe:./bin/clear.exe /Fo:./bin/clear.obj
